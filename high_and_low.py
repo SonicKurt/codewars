91561bc3372b036e4936cfb13fd4daa7624e4316
Kurt <kurtcamp@icloud.com>
2023-08-25 15:37:06 -0400

    commented on creating the numbers array

--- high_and_low.py
+++ high_and_low.py
@@ -9,8 +9,11 @@ import re
 
 def high_and_low(numbers):
     # Grab each number from the numbers string and store it into an array.
-    num_array = [int(num) for num in re.findall('-?\d+', numbers)]
-    
+    # NOTE: The below statement will also work. However, you will need to construct
+    # a RegEx expression for this statement to work properly.
+    # num_array = [int(num) for num in re.findall('-?\d+', numbers)]
+    num_array = [int(num) for num in numbers.split(" ")]
+
     # Sort the numbers in ascending order
     num_array.sort()
     
