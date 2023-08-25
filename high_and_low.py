49fc2f35a52c96c749f4a252e512e5fdf09b30dc
Kurt <kurtcamp@icloud.com>
2023-08-25 15:34:42 -0400

    completed the highest and lowest challenge

---
+++ high_and_low.py
@@ -0,0 +1,22 @@
+'''
+Highest and Lowest
+
+Author: Kurt Campbell
+Created: 25 August 2023
+'''
+
+import re
+
+def high_and_low(numbers):
+    # Grab each number from the numbers string and store it into an array.
+    num_array = [int(num) for num in re.findall('-?\d+', numbers)]
+    
+    # Sort the numbers in ascending order
+    num_array.sort()
+    
+    # Grab the lowest number and highest number
+    low_num = num_array[0]
+    high_num = num_array[len(num_array) - 1]
+
+    # Create a string that has the highest number as the first number and the lowest number as the last character.
+    return f'{high_num} {low_num}'
